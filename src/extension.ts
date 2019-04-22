8a39fe4006d384bf2e0b3ac83f6d318166e4a5cc
Marik-D <maretskii@gmail.com>
2019-04-22 13:41:39 +0300

    Multiple changes
    * Add typescript compiler options
    * Add information and error messages
    * Clear cache for all codemod subdirectories
    * Pass editor selection to codemod

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,71 @@
 import * as vscode from "vscode";
 import * as jscodeshift from "jscodeshift";
 import { readdir } from "fs";
-import { join } from "path";
+import { join, dirname } from "path";
 import { promisify } from "util";
+import { register } from "ts-node";
 
 export function activate(context: vscode.ExtensionContext) {
-  require("ts-node").register();
+  register({
+    compilerOptions: {
+      allowJs: true,
+      resolveJsonModule: true,
+      esModuleInterop: true,
+      target: "es2015",
+      lib: ["esnext", "dom"],
+      skipLibCheck: true
+    }
+  });
 
   let disposable = vscode.commands.registerCommand(
     "extension.codemod",
     async () => {
+      console.log("Starting codemod command");
       const editor = vscode.window.activeTextEditor;
-      if (editor) {
+      if (!editor) {
+        console.warn("no active editor");
+        return;
+      }
+      try {
         const source = editor.document.getText();
+        const codemods = await enumerateCodemods();
+        if (codemods.length === 0) {
+          vscode.window.showInformationMessage("No codemods were found");
+          return;
+        }
+        const selection = await showSelectionMenu(codemods);
+        if (!selection || selection.length === 0) {
+          vscode.window.showInformationMessage("No selection");
+          return;
+        }
+        const codemodPath = join(
+          vscode.workspace.rootPath!,
+          "codemods",
+          selection
+        );
+
         try {
-          const codemods = await enumerateCodemods();
-          if (codemods.length === 0) {
-            return;
-          }
-          const selection = await showSelectionMenu(codemods);
-          if (!selection) {
-            return;
-          }
-          const codemodPath = join(
-            vscode.workspace.rootPath!,
-            "codemods",
-            selection
+          const res = await execCodemod(
+            codemodPath,
+            {
+              source,
+              path: editor.document.fileName
+            },
+            editor.selection.start
           );
-          const res = await execCodemod(codemodPath, {
-            source,
-            path: editor.document.fileName
-          });
 
           if (res) {
             await replaceBuffer(res);
+          } else {
+            vscode.window.showInformationMessage("File skipped");
           }
         } catch (err) {
-          vscode.window.showErrorMessage(err.message);
+          vscode.window.showErrorMessage(
+            `Transform error:\n${err.message}\n${err.stack}`
+          );
         }
+      } catch (err) {
+        vscode.window.showErrorMessage(err.message);
       }
     }
   );
@@ -65,9 +93,7 @@ async function enumerateCodemods() {
     return [];
   }
   try {
-    const files = await promisify(readdir)(
-      join(vscode.workspace.rootPath, "codemods")
-    );
+    const files = await promisify(readdir)(getCodemodsDir()!);
     return files;
   } catch (err) {
     vscode.window.showErrorMessage(
@@ -79,25 +105,43 @@ async function enumerateCodemods() {
 
 async function execCodemod(
   path: string,
-  fileInfo: jscodeshift.FileInfo
+  fileInfo: jscodeshift.FileInfo,
+  position?: vscode.Position
 ): Promise<string | null> {
   const withParser = jscodeshift.withParser("tsx");
-  delete require.cache[require.resolve(path)];
+
+  for (const cacheKey of Object.keys(require.cache)) {
+    if (require.resolve(cacheKey).startsWith(dirname(getCodemodsDir()!))) {
+      delete require.cache[cacheKey];
+    }
+  }
+
   const module = require(path);
 
   const transformer = module.default || module;
 
-  try {
-    return transformer(fileInfo, {
+  console.log("Running codemod", path);
+  const res = transformer(
+    fileInfo,
+    {
       jscodeshift: withParser,
       j: withParser
-    });
-  } catch (err) {
-    vscode.window.showErrorMessage(`Transform error:\n${err.message}`);
-    return null;
-  }
+    },
+    position && {
+      line: position.line + 1,
+      column: position.character
+    }
+  );
+  console.log("Finished running", path);
+  return res;
 }
 
 async function showSelectionMenu(codemods: string[]) {
   return vscode.window.showQuickPick(codemods);
+}
+
+function getCodemodsDir() {
+  return (
+    vscode.workspace.rootPath && join(vscode.workspace.rootPath, "codemods")
+  );
 }
